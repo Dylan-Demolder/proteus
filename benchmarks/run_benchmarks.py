8a7e3298af8c372107192ac01df95d39caef41f8
Dylan Demolder <dylan.m.demolder@gmail.com>
2026-06-16 05:11:39 +0000

    fix: src/ layout for PyPI packaging, fix test/benchmark paths

--- benchmarks/run_benchmarks.py
+++ benchmarks/run_benchmarks.py
@@ -11,7 +11,7 @@ import sys
 import time
 from pathlib import Path
 
-sys.path.insert(0, os.path.expanduser("~/.hermes"))
+sys.path.insert(0, os.path.expanduser("~/.hermes/proteus/src"))
 
 from proteus import compress_tool_output, compress_summary_line
 from proteus.ccr import stats as ccr_stats
